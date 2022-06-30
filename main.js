0a263fe9f35dcc7c2daae817510cf82c21c9d7dc
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-06-30 12:08:23 -0500

    add mobile menu

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 const sectionSpeakers = document.querySelector('.speaker-wrap');
+const hamburguer = document.querySelector('.hamburguer');
+const menu = document.querySelector('.mobile-menu');
+
+hamburguer.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+const closing = document.querySelector('.close');
+
+closing.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+const closingItem = document.querySelector('.mobile-nav');
+
+closingItem.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
 
 // SPEAKERS INFORMATION //
 
